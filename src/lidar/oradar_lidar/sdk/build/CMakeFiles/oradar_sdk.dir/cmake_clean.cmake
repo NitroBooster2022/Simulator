fb719e08e8908cd1e0502d4e32c9ae8d0e3a2e04
slsecrets357 <xi.yang.li@mail.mcgill.ca>
2024-04-10 20:22:34 -0400

    lidar

---
+++ src/lidar/oradar_lidar/sdk/build/CMakeFiles/oradar_sdk.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oradar_sdk.dir/core/base/timer.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/core/network/ActiveSocket.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/core/network/PassiveSocket.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/core/network/SimpleSocket.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/core/serial/serial.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/core/serial/impl/unix/lock.c.o"
+  "CMakeFiles/oradar_sdk.dir/core/serial/impl/unix/list_ports_linux.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/core/serial/impl/unix/unix_serial.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/src/ord_lidar_c_api_driver.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/src/ord_lidar_driver.cpp.o"
+  "CMakeFiles/oradar_sdk.dir/src/filters/FullScanFilter.cpp.o"
+  "liboradar_sdk.pdb"
+  "liboradar_sdk.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/oradar_sdk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/lidar/oradar_lidar/sdk/build/samples/CMakeFiles/non-blocking_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/non-blocking_test.dir/non-blocking_test.cpp.o"
+  "../non-blocking_test.pdb"
+  "../non-blocking_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/non-blocking_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
